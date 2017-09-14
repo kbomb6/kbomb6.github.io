d2ec9b0281c9662fc1a700bddc21fb8812c2284f
Bauman Family <baumanimac@MacBook-Pro.local>
2017-09-14 14:25:08 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,97 @@
+//jQuery to collapse the navbar on scroll
+var header_height  = $('.navbar').height(),
+    intro_height    = $('.intro-section').height(),
+    offset_val = intro_height + header_height;
+$(window).scroll(function() {
+  var scroll_top = $(window).scrollTop();
+    if (scroll_top >= offset_val) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+            $(".navbar-fixed-top").removeClass("navbar-transparent");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      $(".navbar-fixed-top").addClass("navbar-transparent");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $anchor[0].baseURI = this.href;
+
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+// //jQuery to collapse the navbar on scroll
+// $(window).scroll(function() {
+//     if ($(".navbar").offset().top > 100) {
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//             $(".navbar-fixed-top").removeClass("navbar-transparent");
+//     } else {
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//       $(".navbar-fixed-top").addClass("navbar-transparent");
+//     }
+// });
+
+
+//function([string1, string2],target id,[color1,color2])   
+ consoleText(["Hi, I'm Kelly.", "A front end web developer.", "Let's get to work."], 'text',['tomato','rebeccapurple','#00a3cc']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var iteration = 0;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      iteration += 1;
+      if (iteration > 2) {
+        return;
+      }
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 180)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
