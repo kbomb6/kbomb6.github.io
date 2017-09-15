506a0bb57d017dc0a612b264f19d9c7f0eb49f87
Bauman Family <baumanimac@MacBook-Pro.local>
2017-09-15 10:40:09 -0600

    added spree js

--- main.js
+++ main.js
@@ -95,3 +95,22 @@ function consoleText(words, id, colors) {
   }, 400)
 }
 
+// form submission
+
+$('form.contact_form').validate({
+  messages: { },
+  submitHandler: function(form) {
+    $.ajax({
+      url: "//formspree.io/bauman.kellyk@gmail.com",
+      method: "POST",
+      data: $(form).serialize(),
+      dataType: "json",
+      success: function(data) {
+          $("form.contact_form :input").prop("disabled", true);
+          $('#thanks').show();
+      }
+    });
+    return false;
+  }
+});
+
